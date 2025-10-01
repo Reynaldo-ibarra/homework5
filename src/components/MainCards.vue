9da1707f0b6b97725bdbaf12e26301b1aee437f9
Reynaldo Ibarra <ibarra.rey02@gmail.com>
2025-09-30 19:27:20 -0500

    Card data and style done

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -4,9 +4,10 @@ import MainCardSingle from './MainCardSingle.vue';
 </script>
 
 <template>
-    <MainCardSingle v-for="card in 20" :key="card">
+    <div class="grid grid-cols-5 gap-10 p-10">
+        <MainCardSingle v-for="card in 20" :key="card"></MainCardSingle>
+    </div>
 
-    </MainCardSingle>
 
 
 </template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,9 @@
 <script setup>
 //import { ref } from 'vue'
 //const title = ref('Hello There ✋')
-import MainCards from '@/components/MainCards.vue'
+import MainCards from '@/components/MainCards.vue';
 </script>
 
 <template>
-  <MainCards>
-
-  </MainCards>
+  <MainCards></MainCards>
 </template>
